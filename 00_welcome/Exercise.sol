be5f24a487307d51396c22451f6c7068b85c2b62
Geoffrey Moya <geoffrey.moya@gmail.com>
2024-09-17 12:35:25 +0000

    exercice 0 et 1

--- 00_welcome/Exercise.sol
+++ 00_welcome/Exercise.sol
@@ -4,5 +4,10 @@ pragma solidity 0.8.16;
 import "forge-std/Script.sol";
 
 contract Exercise {
-    function _remove(uint256[] storage _arr, uint256 _index) internal {}
+    function _remove(uint256[] storage _arr, uint256 _index) internal {
+		for (uint256 i = _index; i < _arr.length - 1; i++) {
+            _arr[i] = _arr[i + 1];
+        }
+		_arr.pop();
+	}
 }
--- 01_delegation/Exercise.sol
+++ 01_delegation/Exercise.sol
@@ -2,7 +2,31 @@
 pragma solidity 0.8.16;
 
 import "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import "./ICounter.constant.sol";
 
 contract Exercise {
-    function setCounter(address _counter) external {}
+
+	address public immutable me;
+    address internal _me;
+	
+	uint256 public counter;
+
+	address public counter_addr;
+	
+	constructor() {
+       	me = address(this);
+       	_me = address(this);
+   	}	
+
+    function setCounter(address _counter) external {
+		counter_addr =_counter;   
+	}
+
+	function inc() external {
+	 	(bool success, bytes memory data) = counter_addr.delegatecall(
+         	 abi.encodeWithSignature("inc()") 
+		);
+	}
+	
 }
